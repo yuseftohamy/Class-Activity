00a0fdbf4da88a916ef5d58f719c97345f4b89f5
Yusef Tohamy <tuo13985@temple.edu>
2025-01-28 12:13:55 -0500

    Implemented action when user clicks button.

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +9,16 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var clickMeButton : Button
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeButton = findViewById<Button>(R.id.button)
+
+        clickMeButton.setOnClickListener {
+            clickMeButton.text = "Button Clicked!"
+        }
     }
 }
